0dfb74da02642733350c94ec79eca543f6059487
shawnlaffan <shawnlaffan@gmail.com>
2021-09-09 11:51:58 +1000

    Further dedup experiments
    Need to avoid creating excess ndarrays

--- etc/experiment/Util/check.pl
+++ etc/experiment/Util/check.pl
@@ -6,7 +6,9 @@ use blib;
 use Statistics::Descriptive::PDL::Util;
 use PDL;
 
-my $d = pdl [1..6];
-my $w = pdl [1,1,1,2,2,2];
+my $d = pdl [1..10];
+my $w = pdl [1,1,1,2,2,2,3,3,3,4];
 say qq{$w $d};
-say sum_a_by_contiguous_b ($d, $w)
+my ($t) = sum_a_by_contiguous_b ($d, $w);
+
+say "OUTPUT: $t";
