200ec60da321efc9c3f14263477102647a59f3ba
shawnlaffan <shawnlaffan@gmail.com>
2021-09-09 16:51:20 +1000

    dedup variant using scalar and slice instead of where

--- etc/experiment/Util/check.pl
+++ etc/experiment/Util/check.pl
@@ -9,14 +9,14 @@ use PDL;
 my $d = pdl [1..10];
 my $w = pdl [1,1,1,2,2,2,3,3,3,4];
 say qq{$w $d};
-my ($t) = rle_sum ($d, $w);
+my ($t) = rle_sum_onesie ($d, $w);
 
 say "OUTPUT: $t";
 
 $d = pdl ([1..10], [10..19]);
 $w = pdl ([1,1,1,2,2,2,3,3,3,4],
-          [1,1,1,2,2,2,3,3,3,4]);
+          [1,1,2,2,2,2,3,3,3,4]);
 say qq{$w $d};
-my ($t) = rle_sum ($d, $w);
+my ($t) = rle_sum_onesie ($d, $w);
 
 say "OUTPUT: $t";
