e948d8af19ebb2bb5fd7927df2796204255b1887
shawnlaffan <shawnlaffan@gmail.com>
2021-09-09 12:13:41 +1000

    Clean up comments in pd file

--- etc/experiment/Util/check.pl
+++ etc/experiment/Util/check.pl
@@ -12,3 +12,11 @@ say qq{$w $d};
 my ($t) = sum_a_by_contiguous_b ($d, $w);
 
 say "OUTPUT: $t";
+
+$d = pdl ([1..10], [1..10]);
+$w = pdl ([1,1,1,2,2,2,3,3,3,4],
+             [1,1,1,2,2,2,3,3,3,4]);
+say qq{$w $d};
+my ($t) = sum_a_by_contiguous_b ($d, $w);
+
+say "OUTPUT: $t";
