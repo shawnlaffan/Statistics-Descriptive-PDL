43ceffff5a480b2bfbc16aef7de4552634b87c17
shawnlaffan <shawnlaffan@gmail.com>
2021-09-09 12:32:28 +1000

    rename sub to rle_sum, and more cleanup

--- etc/experiment/Util/check.pl
+++ etc/experiment/Util/check.pl
@@ -9,14 +9,14 @@ use PDL;
 my $d = pdl [1..10];
 my $w = pdl [1,1,1,2,2,2,3,3,3,4];
 say qq{$w $d};
-my ($t) = sum_a_by_contiguous_b ($d, $w);
+my ($t) = rle_sum ($d, $w);
 
 say "OUTPUT: $t";
 
-$d = pdl ([1..10], [1..10]);
+$d = pdl ([1..10], [10..19]);
 $w = pdl ([1,1,1,2,2,2,3,3,3,4],
-             [1,1,1,2,2,2,3,3,3,4]);
+          [1,1,1,2,2,2,3,3,3,4]);
 say qq{$w $d};
-my ($t) = sum_a_by_contiguous_b ($d, $w);
+my ($t) = rle_sum ($d, $w);
 
 say "OUTPUT: $t";
